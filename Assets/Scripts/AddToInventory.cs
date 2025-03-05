272e0106e5d50750c767800cbc7ec7c35a72de21
Amber Hafeez <ambernoelle.h@gmail.com>
2025-03-05 15:24:10 +0000

    Changes to RaycastPlayer script and Inventory system
    - in raycastplayer script, addtoinventory script is accessed to add clues
    - pickup animation is played on click if object has the raycast cube component
    - Added evidence properties script which has the sprite and enemyInt of the clue

--- Assets/Scripts/AddToInventory.cs
+++ Assets/Scripts/AddToInventory.cs
@@ -1,44 +1,50 @@
 using UnityEngine.UI;
 using UnityEngine;
 using TMPro;
+using Unity.VisualScripting;
 
 public class AddToInventory : MonoBehaviour
 {
     public Image[] mugShots;
     public Image[] clues;
     public int[] enemyOrder;
-    public int nextSlot = 0;
+    public int nextSlot;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        nextSlot = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    public void AddClue()
+    public int AddClue(int clueNum, Sprite clueSprite, int enemyInt)
     {
         // clues[nextSlot] is set to the sprite on the object (scriptable object?)
-        //clues[nextSlot].sprite = clue.clueSprite;
+        clues[nextSlot].sprite = clueSprite;
+        clues[nextSlot].gameObject.SetActive(true);
         // mugShots[nextSlot] reveals question mark icon
-        mugShots[nextSlot].enabled = true;
-        // enemy associated with clue is assigned the int nextSlot so its position is stored (in MBSArrestUpdateUI?)
-            // int enemyNum = clue.enemyInt
-            // enemyOrder[nextSlot] = enemyNum;
+        mugShots[nextSlot].gameObject.SetActive(true);
+        Debug.Log(mugShots[nextSlot]);
+        // enemy associated with clue is assigned the int nextSlot so its position is stored
+        enemyOrder[nextSlot] = enemyInt;
+        Debug.Log(enemyOrder[nextSlot]);
         // add 1 to nextSlot
-        nextSlot ++;
+        nextSlot += 1;
+
+        return nextSlot;
     }
 
     
     public void AddEnemy()
     {
         // if enemy is captured, mugShots[enemy.enemyslot] is set to enemy.Image
+
     }
     
 
--- Assets/Scripts/RaycastPlayer.cs
+++ Assets/Scripts/RaycastPlayer.cs
@@ -1,13 +1,17 @@
 using UnityEngine;
 public class RaycastPlayer : MonoBehaviour
 {
-    public Camera playerCamera;
+    Camera playerCamera;
     public float lineRange = 50f;
     public static Vector3 hitPosition;
+    AddToInventory addToInventoryScript;
+    Animator anim;
 
     void Start()
     {
-        
+        playerCamera = Camera.main;
+        anim = GetComponentInChildren<Animator>();
+        addToInventoryScript = FindFirstObjectByType<AddToInventory>();
     }
 
     void Update()
@@ -24,6 +28,16 @@ public class RaycastPlayer : MonoBehaviour
                 hitPosition = hit.point;
                 //Debug.Log("Hit position " + hit.point);
                 Debug.Log("Hit object: " + hit.collider.name);
+            }
+
+            RaycastCube clueObject = hit.transform.GetComponent<RaycastCube>();
+            EvidenceProperties evidenceProps = hit.transform.GetComponent<EvidenceProperties>();
+            if(clueObject != null)
+            {
+                anim.SetTrigger("isPickingUp");
+                Debug.Log("there is a clue");
+                addToInventoryScript.nextSlot = addToInventoryScript.AddClue(clueObject.evidenceValue, evidenceProps.clueIcon, evidenceProps.enemyInt);
+                Destroy(clueObject.gameObject);
             }
         }
     }
