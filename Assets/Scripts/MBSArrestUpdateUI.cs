2c5708ce12982ccc8911e9fdbe5b413b59b088a2
Amber Hafeez <ambernoelle.h@gmail.com>
2025-03-07 15:19:56 +0000

    Added functionality for enemy mugshots to be displayed upon arrest in correct inventory slot

--- Assets/Scripts/MBSArrestUpdateUI.cs
+++ Assets/Scripts/MBSArrestUpdateUI.cs
@@ -4,10 +4,20 @@ public class MBSArrestUpdateUI : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     
+    public Sprite mugshot;
+    int enemyInt;
+    AddToInventory addToInvScript;
+
+    void Start()
+    {
+        enemyInt = GetComponent<MBSCriminalUnID>().intCriminalIndex;
+        addToInvScript = FindFirstObjectByType<AddToInventory>();
+    } 
+
     public void FnArrestUpdateUI(int IntCriminalCaught)
 
     { 
-    
+        addToInvScript.AddEnemy(mugshot, enemyInt);
     
     }
 
