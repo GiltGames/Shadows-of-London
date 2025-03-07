004c7a144a49b2b6f6ea0f6e8b088ecf376bc513
GiltGames <info@giltgames.com>
2025-03-07 17:06:24 +0000

    Adding Prefabs of scripted people

--- Assets/Scripts/MBS Drain.cs
+++ Assets/Scripts/MBS Drain.cs
@@ -3,7 +3,9 @@ using UnityEngine;
 public class MBSDrain : MonoBehaviour
 {
     [SerializeField] PlayerMovement mbsPlayer;
-    [SerializeField] float fltStaminaLoss= 0.1f;
+    [SerializeField] float fltRangetoSee=15f;
+    [SerializeField] bool isVisible;
+    [SerializeField] float fltStaminaLoss= 10f;
     [SerializeField] GameObject gmoGhost;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -15,18 +17,37 @@ public class MBSDrain : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (isVisible)
+        {
+            if ((transform.position - mbsPlayer.transform.position).magnitude > fltRangetoSee)
+            {
+                isVisible = false;
+                gmoGhost.SetActive(false);
+
+
+            }
+
+        }
+
+       else
+        {
+            if ((transform.position - mbsPlayer.transform.position).magnitude < fltRangetoSee)
+            {
+                isVisible = true;
+                gmoGhost.SetActive(true);
+
+            }
+
+
+        }
+
+
+   
+
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        gmoGhost.SetActive(true);
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        gmoGhost.SetActive(false);
-    }
+   
 
 
     private void OnTriggerStay(Collider other)
